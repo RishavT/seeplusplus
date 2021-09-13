23e1730ffb2c901e0cd6ff5ca7109e120dc38087
Rishav Thakker <rishav@nptel.iitm.ac.in>
2021-09-14 01:56:45 +0530

    Marking message as read once read

--- gmail/main.py
+++ gmail/main.py
@@ -8,7 +8,10 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.modify',
+]
 LABEL_ID = sys.argv[1]
 
 def send_notification(text):
@@ -51,21 +54,45 @@ def main():
     send_notification('Looking for OTPs')
     service = build('gmail', 'v1', credentials=creds)
 
-    # Call the Gmail API
-    # results = service.users().labels().list(userId='me').execute()
-    message_id = service.users().messages().list(
-        userId='me', labelIds=LABEL_ID, maxResults=1
-    ).execute()['messages'][0]['id']
+    # Get the latest unread messages in LABEL_ID
+    messages = service.users().messages().list(
+        userId='me', labelIds=[LABEL_ID, 'UNREAD'], maxResults=1
+    ).execute().get('messages')
+    if not messages:
+        send_notification("No OTPs sent recently")
+        return False
+    message_id = messages[0]['id']
 
     message = service.users().messages().get(
         userId='me', id=message_id
     ).execute()['snippet']
 
-    send_notification(f'Got text {message}')
+    # Write message to temporary file
+    with open("/tmp/current_otp_msg", "w") as f:
+        f.write(message)
+
+    # Send notifications and copy to clipboard
+    send_notification(f'Got text: {message}')
     otp = get_otp(message)
     if otp:
         copy_text(otp)
         send_notification(f'Copied otp {otp}')
+    else:
+        send_notification(f'Could not decipher OTP. Opening mousepad with '
+                          'entire message')
+        os.system("mousepad /tmp/current_otp_msg")
+
+    # Mark as read
+    response = service.users().messages().modify(
+        userId='me', id=message_id,
+        body={
+            'removeLabelIds': [ 'UNREAD' ],
+        }
+    ).execute()
+    if not (response['id']  == message_id and 'UNREAD' not in
+            response['labelIds']):
+        msg = "Failed to make unread"
+        send_notification(msg)
 
 if __name__ == '__main__':
     try:
